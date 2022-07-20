54a8607b0c6b0775d22ee798a956ec164c683732
rupeshilogsolution <rupesh.kumar@ilogsolution.com>
2022-07-20 17:16:58 +0530

    LOgin Problem solve

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,35 +1,36 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Awllogo from "../../img/awl2.png";
 import "./Login.css";
 import { UserLogin } from '../../api'
 
 export default function Login() {
-  const [error,setError] =useState(false);
+  const [error, setError] = useState(false);
 
   const handlelogin = async () => {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    if(!username|| !password){
+    if (!username || !password) {
       setError(true)
     }
-    else{
+    else {
       const result = await UserLogin(username, password);
-      console.log(result.name)
-      if(result){
-        sessionStorage.setItem('UserName',result.name);
-        sessionStorage.setItem('UserId',result.user_id);
-        sessionStorage.setItem('Token',result.token);
-        sessionStorage.setItem('Permission',result.permission)
-        window.location.href='./Dashboard'
+      console.log(result)
+      if (result.status==='Success') {
+        console.log("if")
+        sessionStorage.setItem('UserName', result.name);
+        sessionStorage.setItem('UserId', result.user_id);
+        sessionStorage.setItem('Token', result.token);
+        sessionStorage.setItem('Permission', result.permission)
+        // window.location.href='./Dashboard'
       }
-      else{
-        alert('Invalid Username and password')
-  
+      else {
+        console.log('else')
+        alert(`Invalid Username:-${username} and password:-${password}`);
       }
-      
+
     }
 
-   
+
 
   }
 
@@ -45,8 +46,8 @@ export default function Login() {
               <input type="text" id="username" placeholder="Username" ></input>
               <input type="password" id="password" placeholder="Password" ></input>
               {
-                error?<p style={{color:"red"}}>Please Enter Userid & Password ...</p>:null
-              } 
+                error ? <p style={{ color: "red" }}>Please Enter Userid & Password ...</p> : null
+              }
               <button className="btn btn-primary mt-2 psw-btn" value="Login" onClick={handlelogin}>Login</button>
             </div>
           </div>
