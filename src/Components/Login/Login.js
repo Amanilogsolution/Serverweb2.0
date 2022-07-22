3250642879082ce1dc578bc73407b33211f9ebbd
rupeshilogsolution <rupesh.kumar@ilogsolution.com>
2022-07-22 10:05:45 +0530

    login page password toggle done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,6 +5,7 @@ import { UserLogin } from '../../api'
 
 export default function Login() {
   const [error, setError] = useState(false);
+  const [toggleeye, setToggleeye] = useState(true);
 
   const handlelogin = async () => {
     const username = document.getElementById('username').value;
@@ -32,17 +33,36 @@ export default function Login() {
   }
 
 
+  const togglefun = () => {
+    console.log('click')
+    setToggleeye(!toggleeye);
+  }
+
   return (
     <>
       <div className="formmaindiv">
         <div className="formcontainer" >
           <div className="login">
             <img className="p-0" id="img" src={Awllogo} alt="awl india" />
-            <h4 className="text-center heading-title mt-2" >AWL India Serversite</h4>
+            <h4 className="text-center heading-title mt-2" >AWL India <b>IPERISCOPE</b></h4>
             <div className="control">
               <input type="text" id="username" placeholder="Username" ></input>
-              <input type="password" id="password" placeholder="Password" ></input>
-              {
+              <div className="form-row">
+
+                <input type={toggleeye?'password':'text'} id="password" placeholder="Password" className="form-group col-md-11" ></input>
+                {
+                  toggleeye ?
+                    <span className="material-symbols-outlined form-group col-md-1 mt-2 eyebtn" onClick={togglefun}>
+                      visibility_off
+                    </span> : <span className="material-symbols-outlined form-group col-md-1 mt-2 eyebtn" onClick={togglefun}>
+                      visibility
+                    </span>
+                }
+
+
+
+              </div>
+              { 
                 error ? <p style={{ color: "red" }}>Please Enter Userid & Password ...</p> : null
               }
               <button className="btn btn-primary mt-2 psw-btn" value="Login" onClick={handlelogin}>Login</button>
