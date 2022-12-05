2fa40980ab46679e874c826a86f0e08a28a42b96
Amanilogsolution <aman@ilogsolution.com>
2022-12-05 18:28:01 +0530

    push

--- src/components/pages/Dashboard/Outstanding/Outstandingdetails.js
+++ src/components/pages/Dashboard/Outstanding/Outstandingdetails.js
@@ -2,7 +2,128 @@ import './Outstandingdetail.css'
 const Outstatndingdetails = () => {
     return (
         <>
-            <h1>Outstatndingdetails</h1>
+            <div className="Outstanding_container_details d-flex justify-content-between mt-2">
+                <div className="card1 flex-fill ">
+                    <p className="bg-dark text-center text-white">Search Vendor Invoice</p>
+                    <div className="d-flex justify-content-center">
+                        <input className=" form-control w-50" type="search" placeholder="Search ..." />
+                        <button class=" btn btn-outline-success " type="submit">Search</button>
+                    </div>
+                </div>
+                <div className="card1 flex-fill">
+                    <p className="bg-dark text-center text-white">Search Payment Detail</p>
+                    <div className="d-flex justify-content-center">
+
+                        <input className=" form-control w-50" type="search" placeholder="Search ..." />
+                        <button class="  btn btn-outline-success " type="submit">Search</button>
+                    </div>
+                </div>
+                <div className="card1 flex-fill">
+                    <p className="bg-dark text-center text-white">Search Billing Account Number</p>
+                    <div className="d-flex justify-content-center">
+
+                        <input className=" form-control w-50" type="search" placeholder="Search ..." />
+                        <button class="  btn btn-outline-success " type="submit">Search</button>
+                    </div>
+                </div>
+
+            </div>
+            <div className="d-flex flex-column mt-2" style={{ minHeight: "60vh" }}>
+                <div style={{ maxHeight: "30vh" }}>
+                    <p className="bg-dark text-center text-white">Vendor Invoices - Detailed</p>
+                    <div style={{ overflow: "auto", height: "20vh" }}>
+                        <table class="table table-striped" >
+                            <thead>
+                                <tr>
+                                    <th scope="col">Company</th>
+                                    <th scope="col">Vendor</th>
+                                    <th scope="col">SubCat</th>
+                                    <th scope="col">Inv</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">ILOG</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">ILOG</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">ILOG</th>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">ILOG</th>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>  <tr>
+                                    <th scope="row">ILOG</th>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div style={{ minHeight: "30vh" }}>
+                    <p className="bg-dark text-center text-white">Paid Invoices - Detailed</p>
+                    <div style={{ overflow: "auto", height: "20vh" }}>
+                        <table class="table table-striped" >
+                            <thead>
+                                <tr>
+                                    <th scope="col">Company</th>
+                                    <th scope="col">Vendor</th>
+                                    <th scope="col">SubCat</th>
+                                    <th scope="col">Inv</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">ILOG</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">ILOG</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">ILOG</th>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">ILOG</th>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>  <tr>
+                                    <th scope="row">ILOG</th>
+                                    <td>Larry</td>
+                                    <td>the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+
         </>
     )
 }
