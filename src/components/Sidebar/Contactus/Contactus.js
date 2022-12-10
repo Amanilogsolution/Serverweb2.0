8009a02ad4aa52ec13fd7709396629f61c07bd60
Akash2001k <kaushikakash116@gmail.com>
2022-12-10 12:42:18 +0530

    Done

--- src/components/Sidebar/Contactus/Contactus.js
+++ src/components/Sidebar/Contactus/Contactus.js
@@ -1,8 +1,25 @@
-const Contactus = () => {
+import React from 'react'
+import Sidebar from '../Sidebar'
+
+export default function Contactus() {
     return (
         <>
-            <h1>Contact us</h1>
+            <Sidebar>
+                <div className='main_container px-3 py-3 px-sm-5 py-sm-5'>
+                    <div className='card w-50' style={{border:"none",boxShadow:"1px 1px 4px 1px silver"}}>
+                        <div className='card-header' style={{border:"none",background:"#eff3f6"}}>
+                            <h5>Contact Us</h5>
+                        </div>
+                        <div className='card-body px-4 py-4'>
+                            <h5>Support Phones</h5>
+                            <p> US Enterprise: +1.877.422.3865</p>
+                            <p style={{marginTop:"-14px"}}>US MSP: +1.877.422.3865</p>
+                            <h5>Support Email</h5>
+                            <a class="nav-link" aria-expanded="false">support@itarian.com</a>
+                        </div>
+                    </div>
+                </div>
+            </Sidebar>
         </>
     )
 }
-export default Contactus;
--- src/components/Sidebar/Navbar.js
+++ src/components/Sidebar/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = (propes) => {
                             <p className='mb-0 mx-1' style={{ fontSize: "22px" }}>Setting</p>
                             <MdOutlineArrowDropDown style={{ fontSize: "30px", margin: "-2px 0" }} />
                         </div>
-                        <div className="dropdown-menu border border-dark" aria-labelledby="dropdownMenuButton" style={{ boxShadow: '5px 5px 10px #000' }}>
+                        <div className="dropdown-menu " aria-labelledby="dropdownMenuButton" style={{ boxShadow: '1px 1px 3px gray' }}>
                             <Link className="dropdown-item" to="/TotalRoles">Role</Link>
                             <Link className="dropdown-item" to="#">Account</Link>
                             <Link className="dropdown-item" to="#">Appliction</Link>
@@ -52,7 +52,7 @@ const Navbar = (propes) => {
                                 <BsFillClockFill style={{ fontSize: "25px" }} />
                                 <MdOutlineArrowDropDown style={{ fontSize: "30px", margin: "-2px 0" }} />
                             </div>
-                            <div className="dropdown-menu border border-dark" aria-labelledby="dropdownMenuButton" style={{ boxShadow: '5px 5px 10px #000' }}>
+                            <div className="dropdown-menu " aria-labelledby="dropdownMenuButton" style={{ boxShadow: '1px 1px 3px gray' }}>
                                 <a className="dropdown-item" href="/AddNewAssets">Enroll New Asset</a>
                                 <Link className="dropdown-item" to="/AddTickets">Create a New Ticket</Link>
                                 <Link className="dropdown-item" to="/AddVendorCode">Create a New Vendor</Link>
@@ -64,7 +64,7 @@ const Navbar = (propes) => {
                                 <MdOutlineHelp style={{ fontSize: "27px" }} />
                                 <MdOutlineArrowDropDown style={{ fontSize: "30px", margin: "-2px 0" }} />
                             </div>
-                            <div className="dropdown-menu border border-dark" aria-labelledby="dropdownMenuButton" style={{ boxShadow: '5px 5px 10px #000' }}>
+                            <div className="dropdown-menu " aria-labelledby="dropdownMenuButton" style={{ boxShadow: '1px 1px 3px gray' }}>
                                 <Link className="dropdown-item" to="#"><BsYoutube /> Quick Start Video</Link>
                                 <Link className="dropdown-item" to="#"> <MdOutlineHelp /> Getting Started Guide</Link>
                                 <Link className="dropdown-item" to="#"><MdGroups style={{ fontSize: '25px' }} /> Free Onbording Session</Link>
@@ -84,7 +84,7 @@ const Navbar = (propes) => {
                             <p className=' mb-0 name-text' style={{ fontSize: '18px' }}>{sessionStorage.getItem('UserName')}</p>
                             <MdOutlineArrowDropDown style={{ fontSize: "30px" }} />
                         </div>
-                        <div className="dropdown-menu border border-dark" aria-labelledby="dropdownMenuButton" style={{ boxShadow: '5px 5px 10px #000' }}>
+                        <div className="dropdown-menu " aria-labelledby="dropdownMenuButton" style={{ boxShadow: '1px 1px 3px gray' }}>
                             <a className="dropdown-item" href="/Profile">Profile</a>
                             <Link className="dropdown-item" to="/ChangePassword">Change Password</Link>
                             <a className="dropdown-item border-top text-danger" href="#" onClick={handleLogout}>Logout <MdOutlineExitToApp style={{ fontSize: "20px" }} /></a>
