c181ca7cbf112746fbb2e68a1764b0750c0dca9a
Amanilogsolution <aman@ilogsolution.com>
2022-12-21 15:56:37 +0530

    Organisation Details

--- src/api/index.js
+++ src/api/index.js
@@ -679,7 +679,7 @@ export const UpdateLocationStatus = async (org,status, sno) => {
     return axios.post(url, { org,status, sno }).then(response => response.data).catch(error => console.log(error));
 }
 
-export const AddLocationapi = async (org,location_id, company_name, location_code, location_name, location_address_line1, location_address_line2, location_city, location_state, location_pin_code, location_gst, contact_person, contact_person_email, contact_person_number, location_latitude, location_longitude, user_id) => {
+export const AddLocationapi = async (org,location_id, company_name, location_code, location_name, location_address_line1, location_address_line2, location_city, location_state, location_pin_code, location_gst, contact_person, contact_person_email, contact_person_number, location_latitude, location_longitude, user_id,location_country) => {
     const url = `https://drizzlebackend.awlworldwide.com/api/insertLocation`
     return axios.post(url, {org, location_id, company_name, location_code, location_name, location_address_line1, location_address_line2, location_city, location_state, location_pin_code, location_gst, contact_person, contact_person_email, contact_person_number, location_latitude, location_longitude, user_id }).then(response => response.data).catch(error => console.log(error));
 }
