6e4a2f562f21e0170613b4b1351fd74e302e9213
rupeshilogsolution <rupesh.kumar@ilogsolution.com>
2023-01-05 16:05:16 +0530

    Done

--- src/components/pages/Dashboard/Outstanding/Outstanding.js
+++ src/components/pages/Dashboard/Outstanding/Outstanding.js
@@ -9,12 +9,12 @@ import ReactPaginate from 'react-paginate';
 
 import 'react-data-table-component-extensions/dist/index.css';
 import { AiFillEdit } from 'react-icons/ai';
-import { Invoice_Outstanding,TotalOutstanding } from '../../../../api/index'
+import { Invoice_Outstanding, TotalOutstanding } from '../../../../api/index'
 
 const Outstanding = ({ setStep }) => {
   const [invoices, setInvoice] = useState({})
   const [outstandingAmount, setOutstandingAmount] = useState()
-  const [outstandingDatas,setOutstandingdatas] = useState([])
+  const [outstandingDatas, setOutstandingdatas] = useState([])
   const [TotalVendor, setTotalVendor] = useState([])
   const [rowperpage, setRowPerPage] = useState(10)
   const [lastval, setLastval] = useState()
@@ -38,18 +38,18 @@ const Outstanding = ({ setStep }) => {
       fetch()
 
 
-    const org = localStorage.getItem('Database')
+      const org = localStorage.getItem('Database')
 
-    const datas = await TotalOutstanding(org, 1, 10)
-    console.log(datas)
-    setTotalVendor(datas.data)
-    console.log(datas)
-    const total = datas.TotalData[0]["Totaldata"]
-    setRowPerPage(10)
-    setLastval(Math.ceil(total / 10))
-  }
-  fetchdata()
-   
+      const datas = await TotalOutstanding(org, 1, 10)
+      console.log(datas)
+      setTotalVendor(datas.data)
+      console.log(datas)
+      const total = datas.TotalData[0]["Totaldata"]
+      setRowPerPage(10)
+      setLastval(Math.ceil(total / 10))
+    }
+    fetchdata()
+
   }, [])
 
   const handleChange = async (e) => {
@@ -67,7 +67,7 @@ const Outstanding = ({ setStep }) => {
     const Outstanding = await Invoice_Outstanding(localStorage.getItem('Database'))
     console.log(Outstanding.OutstandingVendor)
     setOutstandingdatas(Outstanding.OutstandingVendor)
-   
+
     setInvoice(Outstanding.Vendor)
     let money = Outstanding.OutstandingAmount.toLocaleString('en-IN');
     console.log(money)
@@ -101,7 +101,7 @@ const Outstanding = ({ setStep }) => {
   // };
   return (
     <section className='outstanding-container d-flex justify-content-around mt-2'>
-      <div className='oustanding-details '>
+      <div className='oustanding-details border border-danger'>
         <div className='outstanding-top-detail-div d-flex justify-content-between text-center'>
           <div className='outstanding-totalinv border border-dark'>
             <p className='outstanding-totalhead text-white'>Invoices</p>
@@ -112,38 +112,64 @@ const Outstanding = ({ setStep }) => {
             <p className='outstanding-value mb-2'>₹{outstandingAmount}</p>
           </div>
         </div>
-        <div style={{ width: "100%", marginTop: "4px", textAlign: "center", color: "white", background: "black" }}>ILOG- IT OUTSTANDING
-        </div>
-        <div className='company-outstatnding border border-dark' style={{ maxHeight: "40vh" }}>
-          <div className="d-flex justify-content-between" style={{ background: "white", position: "fixed", width: "380px", maxHeight: "25px" }}>
+
+        <div className='company-outstatnding border border-dark ' >
+          <p className='bg-dark text-white text-center mt-2 mb-0'>ILOG- IT OUTSTANDING</p>
+          {/* <div className="d-flex justify-content-between" style={{ background: "white", position: "fixed", width: "380px", maxHeight: "25px" }}>
             <p>Vedor</p>
             <p><b>Amount</b></p>
 
-          </div>
-          <div style={{ maxHeight: "32vh", overflow: "auto",marginTop:"30px" }}>
-            <table className="table text-center table-striped" >
-              <thead className="thead-dark">
-              
-              </thead>
-              <tbody className='text-center' >
-              {
-                outstandingDatas.map(element => (
-                  <tr>
-                  <td>{element.name}</td>
-                  <th>{element.value}</th>
+          </div> */}
+          <div style={{ maxHeight: "32vh", overflow: "auto", border: '2px solid red' }}>
+            <table className="table table-striped" >
+              <thead className="bg-white position-sticky top-0" >
+                <tr>
+                  <th>Vendor</th>
+                  <th>Amt</th>
                 </tr>
-                ))
-              } 
+
+              </thead>
+              <tbody >
+                {
+                  outstandingDatas.map(element => (
+                    <tr>
+                      <td>{element.name}</td>
+                      <th>{element.value}</th>
+                    </tr>
+                  ))
+                }
+                <tr>
+                  <td>test</td>
+                  <td>test</td>
+                </tr>
+                <tr>
+                  <td>test</td>
+                  <td>test</td>
+                </tr>
+                <tr>
+                  <td>test</td>
+                  <td>test</td>
+                </tr>
+                <tr>
+                  <td>test</td>
+                  <td>test</td>
+                </tr>
               </tbody>
+              <tfoot>
+                <tr>
+                  <th>Total</th>
+                  <th>₹{outstandingAmount}</th>
+                </tr>
+              </tfoot>
             </table>
           </div>
-          <div className="d-flex justify-content-between" style={{ border: "1px solid black", background: "white", position: "fixed", width: "383px", maxHeight: "25px" }}>
+          {/* <div className="d-flex justify-content-between" style={{ border: "1px solid black", background: "white", position: "fixed", width: "383px", maxHeight: "25px" }}>
             <p>Total</p>
             <p><b>₹{outstandingAmount}</b></p>
 
-          </div>
+          </div> */}
         </div>
-        <div className='nextoutstanding-detail cursor-pointer rounded text-light  mt-5 h-25 d-flex justify-content-center align-items-center' style={{ cursor: "pointer" }} onClick={() => { setStep(5) }}>
+        <div className='nextoutstanding-detail cursor-pointer rounded text-light  mt-2 h-25 d-flex justify-content-center align-items-center' onClick={() => { setStep(5) }}>
           Click for Outstanding Details
         </div>
       </div>
@@ -151,10 +177,10 @@ const Outstanding = ({ setStep }) => {
         <p className='bg-dark text-white d-flex justify-content-between h5 py-1 px-2'>ILOG- IT OUTSTANDING as on DATE
         </p>
         <div>
-        <div title="Export" className="d-flex justify-content-end mr-2" onClick={(e) => { e.preventDefault(); setToogle(value => !value) }} style={{width:"5%",float:"right",position:"relative"}}>
+          <div title="Export" className="d-flex justify-content-end mr-2" onClick={(e) => { e.preventDefault(); setToogle(value => !value) }} style={{ width: "5%", float: "right", position: "relative" }}>
             <BiExport style={{ fontSize: "25px", marginRight: "20px" }} />
           </div>
-          <div style={{ backgroundColor: "#FCFCFC", position: "absolute", right: "2%",top:"35%", width: "5%", boxShadow: "3px 3px 10px black", borderRadius: "5px" }}>
+          <div style={{ backgroundColor: "#FCFCFC", position: "absolute", right: "2%", top: "35%", width: "5%", boxShadow: "3px 3px 10px black", borderRadius: "5px" }}>
             {
               toogle ?
                 <div className="d-flex flex-column justify-content-center align-items-center" >
@@ -178,7 +204,7 @@ const Outstanding = ({ setStep }) => {
                 <th scope="col">Invoice_no</th>
                 <th scope="col">Reference No</th>
                 <th scope="col">Invoice Amt</th>
-               
+
               </tr>
             </thead>
             <tbody>
@@ -190,7 +216,7 @@ const Outstanding = ({ setStep }) => {
                       <td>{elements.invoice_no}</td>
                       <td>{elements.reference_no}</td>
                       <td>{elements.invoice_amt}</td>
-                    
+
                     </tr>
                   )
                 })
@@ -200,38 +226,38 @@ const Outstanding = ({ setStep }) => {
             </tbody>
           </table>
           <div className="d-flex justify-content-end">
-          <div className="d-flex justify-content-center align-items-center mx-2">
-            <label>Rows Per Page</label>
-            <select onChange={handleChange}>
-              <option value="10">10</option>
-              <option value="15">15</option>
-              <option value="20">20</option>
+            <div className="d-flex justify-content-center align-items-center mx-2">
+              <label>Rows Per Page</label>
+              <select onChange={handleChange}>
+                <option value="10">10</option>
+                <option value="15">15</option>
+                <option value="20">20</option>
 
-            </select>
+              </select>
+            </div>
+            <div>
+              <ReactPaginate
+                breakLabel="..."
+                nextLabel="next "
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                pageCount={lastval}
+                previousLabel=" previous"
+                renderOnZeroPageCount={null}
+                containerClassName={'pagination justify-content-end'}
+                pageClassName={'page-item'}
+                pageLinkClassName={'page-link'}
+                previousClassName={'page-item'}
+                previousLinkClassName={'page-link'}
+                nextClassName={'page-item'}
+                nextLinkClassName={'page-link'}
+                breakClassName={'page-item'}
+                breakLinkClassName={'page-link'}
+                activeClassName={'active'}
+              />
+            </div>
           </div>
-          <div>
-            <ReactPaginate
-              breakLabel="..."
-              nextLabel="next "
-              onPageChange={handlePageClick}
-              pageRangeDisplayed={3}
-              pageCount={lastval}
-              previousLabel=" previous"
-              renderOnZeroPageCount={null}
-              containerClassName={'pagination justify-content-end'}
-              pageClassName={'page-item'}
-              pageLinkClassName={'page-link'}
-              previousClassName={'page-item'}
-              previousLinkClassName={'page-link'}
-              nextClassName={'page-item'}
-              nextLinkClassName={'page-link'}
-              breakClassName={'page-item'}
-              breakLinkClassName={'page-link'}
-              activeClassName={'active'}
-            />
-          </div>
-        </div>
-          
+
 
         </div>
       </div>
