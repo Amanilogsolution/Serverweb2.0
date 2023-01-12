ccdbe00e774329fa757a5b79da314b786cb8301f
Akash2001k <kaushikakash116@gmail.com>
2023-01-12 18:21:49 +0530

    done

--- src/components/pages/Dashboard/TicketDash/TicketPriority.js
+++ src/components/pages/Dashboard/TicketDash/TicketPriority.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './TicketDash.css'
 import { PieChart, Pie, Tooltip, Legend, ResponsiveContainer, Cell } from "recharts";
 import {Ticket_Priority} from '../../../../api/index'
+import { BsCaretDownFill } from 'react-icons/bs';
 const TicketPriority = () => {
 
     const [ticketPriority,setTicketPriority] = useState({  })
@@ -52,10 +53,13 @@ const TicketPriority = () => {
 
     return (
         <div className='d-flex  justify-content-center mt-4' style={{ height: "35vh" }}>
-            <div style={{ border: "2px solid silver", height: "32vh", margin: "6px 10px 15px 10px", borderRadius: "3px", width: "26%" }}>
-                <p className=' text-black text-center px-4 mx-2'>LOW</p>
+            <div className='ticket_card rounded'>
+            <div className='d-flex'>
+                  <BsCaretDownFill style={{fontSize:"55px",padding:"12px",borderRadius:"3px",margin:"-15px 14px 0",background:"linear-gradient(45deg, rgb(68, 97, 240), rgb(37, 63, 196))",color:"white"}}/>
+                  <p style={{fontWeight:"600",color:"#6a6a6a",margin:"8px -2px"}}>Low</p>
+                </div>
 
-                <ResponsiveContainer width="100%" aspect={1.8}>
+                <ResponsiveContainer width="100%" aspect={2.2}>
                     <PieChart width={700} height={200}>
                         <Tooltip contentStyle={{ backgroundColor: "rgb(179, 210, 242)" }} />
                         <Tooltip />
@@ -64,16 +68,19 @@ const TicketPriority = () => {
                                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                             ))}
                         </Pie>
-                        <Legend layout="vertical" verticalAlign="center" align="right" />
+                        <Legend layout="horizontal" verticalAlign="bottom" align="center" />
                     </PieChart>
                 </ResponsiveContainer>
 
             </div>
 
-            <div style={{ border: "2px solid silver", height: "32vh", margin: "6px 10px 15px 10px", borderRadius: "3px", width: "26%" }}>
-                <p className=' text-black text-center px-4 mx-2'>NORMAL</p>
+            <div className='ticket_card rounded'>
+            <div className='d-flex'>
+                  <BsCaretDownFill style={{fontSize:"55px",padding:"12px",borderRadius:"3px",margin:"-15px 14px 0",background:"linear-gradient(45deg, rgb(68, 97, 240), rgb(37, 63, 196))",color:"white"}}/>
+                  <p style={{fontWeight:"600",color:"#6a6a6a",margin:"8px -2px"}}>Normal</p>
+                </div>
 
-                <ResponsiveContainer width="100%" aspect={1.8}>
+                <ResponsiveContainer width="100%" aspect={2.2}>
                     <PieChart width={700} height={200}>
                         <Tooltip contentStyle={{ backgroundColor: "rgb(179, 210, 242)" }} />
                         <Tooltip />
@@ -82,16 +89,20 @@ const TicketPriority = () => {
                                 <Cell key={`cell-${index}`} fill={COLORS2[index % COLORS2.length]} />
                             ))}
                         </Pie>
-                        <Legend layout="vertical" verticalAlign="center" align="right" />
+                        <Legend layout="horizontal" verticalAlign="bottom" align="center" />
                     </PieChart>
                 </ResponsiveContainer>
 
             </div>
 
-            <div style={{ border: "2px solid silver", height: "32vh", margin: "6px 10px 15px 10px", borderRadius: "3px", width: "26%" }}>
-                <p className=' text-black text-center px-4 mx-2'>URGENT</p>
+            <div className='ticket_card rounded'>
+            <div className='d-flex'>
+                  <BsCaretDownFill style={{fontSize:"55px",padding:"12px",borderRadius:"3px",margin:"-15px 14px 0",background:"linear-gradient(45deg, rgb(68, 97, 240), rgb(37, 63, 196))",color:"white"}}/>
+                  <p style={{fontWeight:"600",color:"#6a6a6a",margin:"8px -2px"}}>Urgent</p>
+                </div>
 
-                <ResponsiveContainer width="100%" aspect={1.8}>
+
+                <ResponsiveContainer width="100%" aspect={2.2}>
                     <PieChart width={700} height={200}>
                         <Tooltip contentStyle={{ backgroundColor: "rgb(179, 210, 242)" }} />
                         <Tooltip />
@@ -100,7 +111,7 @@ const TicketPriority = () => {
                                 <Cell key={`cell-${index}`} fill={COLORS3[index % COLORS3.length]} />
                             ))}
                         </Pie>
-                        <Legend layout="vertical" verticalAlign="center" align="right" />
+                        <Legend layout="horizontal" verticalAlign="bottom" align="center" />
                     </PieChart>
                 </ResponsiveContainer>
             </div>
