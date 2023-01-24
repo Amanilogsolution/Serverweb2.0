85ad061605881b3136021449098e2fa5ab3aa3fc
rupeshIlogsolution <rupesh.kumar@ilogsolution.com>
2023-01-24 15:27:48 +0530

    Asset Dashboard Responsive Done

--- src/components/pages/Dashboard/AssetsDash.js
+++ src/components/pages/Dashboard/AssetsDash.js
@@ -4,7 +4,6 @@ import { BarChart, PieChart, Pie, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Leg
 import { DashboarProcedure, Dashboard_Location_Name, Dashboard_Software, Dashboard_Manufacture } from '../../../api/index'
 import { BsLaptopFill } from 'react-icons/bs';
 import { HiUsers } from 'react-icons/hi';
-import { FaTicketAlt } from 'react-icons/fa';
 import { MdStickyNote2 } from 'react-icons/md';
 export default function AssetsDash(callback) {
   const [chartdata, setChartData] = useState()
@@ -24,7 +23,6 @@ export default function AssetsDash(callback) {
       const result = await DashboarProcedure(type)
       setAssetData({ ...Assetsdata, TotalAsset: result[0][0].TotalDevice, ActiveAsset: result[1][0].ActiveDevice, RentalAssets: result[2][0].RentalDevice, RentMonth: result[3][0].rent, PurchaseVal: result[4][0].purchase })
       datas()
-
     }
     fetch()
   }, [])
@@ -48,7 +46,6 @@ export default function AssetsDash(callback) {
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-
     return (
       <text x={x} y={y} fontSize='13' fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
         {`${(percent * 100).toFixed(0)}%`}
@@ -56,64 +53,62 @@ export default function AssetsDash(callback) {
     );
   };
 
-  
 
   const COLORS = ['#a82a75', '#c26411', '#122a8a', '#065b73', '#119c03', '#20541a'];
-  
-
 
   return (
-    <div className='AssetDash'>
-      <div className='Asset_cards mb-2'>
-        <div className='card1 rounded cursor-pointer'>
+    <div className='AssetDash my-4'>
+      <div className='Asset_cards mb-2 d-flex justify-content-between w-100'>
+        <div className='card1 bg-white d-flex justify-content-around shadow1-silver rounded py-1'>
           <div className='position-relative  asset-inner-card'>
             <div className='dash_card_icon_div rounded position-absolute'>
-              <BsLaptopFill className='icon ' />
+              <BsLaptopFill className='icon text-white' />
             </div>
           </div>
-          <div style={{ marginLeft: "-50px" }}>
+          <div className=' asset-inner-card2'>
             <h1 className='dash_card_head mb-0 '>{Assetsdata.TotalAsset}</h1>
             <p className='dash_card_para'>Total Devices</p>
           </div>
         </div>
-        <div className='card1 rounded '>
+
+        <div className='card1 bg-white d-flex justify-content-around shadow1-silver  rounded py-1'>
           <div className='position-relative  asset-inner-card'>
             <div className='dash_card_icon_div activedev1 rounded position-absolute'>
-              <HiUsers className='icon' />
+              <HiUsers className='icon text-white' />
             </div>
           </div>
-          <div style={{ marginLeft: "-50px" }}>
+          <div className=' asset-inner-card2'>
             <h1 className='dash_card_head mb-0'>{Assetsdata.ActiveAsset}</h1>
             <p className='dash_card_para'>Active Devices</p>
           </div>
         </div>
-        <div className='card1 rounded '>
+        <div className='card1 bg-white d-flex justify-content-around  rounded shadow1-silver py-1'>
           <div className='position-relative  asset-inner-card'>
             <div className='dash_card_icon_div rentdev1 rounded position-absolute'>
-              <MdStickyNote2 className='icon' />
+              <MdStickyNote2 className='icon text-white' />
             </div>
           </div>
-          <div style={{ marginLeft: "-50px" }}>
+          <div className=' asset-inner-card2'>
             <h1 className='dash_card_head mb-0'>{Assetsdata.RentalAssets}</h1>
             <p className='dash_card_para'>Rental Devices</p>
           </div>
         </div>
       </div>
 
-      <div className='all_graph'>
+      <div className='all_graph d-flex justify-content-between mt-5'>
         <div className='for_graph'>
-          <div className='bar_location' >
+          <div className='bar_location bg-white position-relative rounded' >
             {spinner ?
-              <div class="spinner-border text-primary" style={{ marginTop: "2%", marginLeft: "50%" }} role="status">
-                <span class="sr-only"></span>
+              <div className="spinner-border text-primary" style={{ marginTop: "2%", marginLeft: "50%" }} role="status">
+                <span className="sr-only"></span>
               </div> :
-              <div className='it-bar' style={{width:"95%" ,height:"95%"}}>
+              <div className='it-bar rounded position-absolute'>
                 <ResponsiveContainer >
                   <BarChart width={600} height={300} data={chartdata} margin={{ top: 20, right: 45 }}>
                     <CartesianGrid strokeDasharray='4' vertical={false} />
                     <XAxis tick={{ fill: 'white' }} dataKey="location_code" interval={"preserveStartEnd"} fontSize={12} />
                     <YAxis tick={{ fill: 'white' }} />
-                    <Tooltip contentStyle={{ backgroundColor: "rgba(41,40, 40,0.8)",color:"white",borderRadius:"4px",border:"1px solid rgba(41,40, 40)" }} />
+                    <Tooltip contentStyle={{ backgroundColor: "rgba(41,40, 40,0.8)", color: "white", borderRadius: "4px", border: "1px solid rgba(41,40, 40)" }} />
                     <Bar dataKey="asset" fill="white" barSize={5} radius={30} />
                     <Tooltip />
                   </BarChart>
@@ -121,26 +116,26 @@ export default function AssetsDash(callback) {
               </div>
             }
             <div >
-              <small className='text-secondary position-absolute mx-4' style={{ bottom:'6.5%' }}>IT Asset Allotment Summary</small>
+              <small className='text-secondary position-absolute mx-4' style={{ bottom: '6.5%' }}>IT Asset Allotment Summary</small>
             </div>
           </div>
-          <div className='graph_2nd_row'>
-            <div className='manufacturer_graph'>
+          <div className='graph_2nd_row d-flex position-relative justify-content-between mt-5'>
+            <div className='manufacturer_graph bg-white rounded position-relative'>
               {spinner ?
-                <div class="spinner-border text-success" style={{ marginTop: "10%", marginLeft: "50%" }} role="status">
-                  <span class="sr-only"></span>
+                <div className="spinner-border text-success" style={{ marginTop: "10%", marginLeft: "50%" }} role="status">
+                  <span className="sr-only"></span>
                 </div> :
-                <div className="manu_pie" style={{width:"94%" ,height:"90%"}}>
+                <div className="manu_pie rounded position-absolute">
                   <ResponsiveContainer>
                     <PieChart width={700} height={150}>
-                      
-                      <Tooltip contentStyle={{ backgroundColor: "rgba(41,40, 40,0.8)",color:"white",borderRadius:"3px",border:"1px solid rgba(41,40, 40)" }} />
+
+                      <Tooltip contentStyle={{ backgroundColor: "rgba(41,40, 40,0.8)", color: "white", borderRadius: "3px", border: "1px solid rgba(41,40, 40)" }} />
                       <Pie fill="#8884d8" stroke="none" data={dashboardmanu} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={38} outerRadius={82} label={renderCustomizedLabel} labelLine={false}>
                         {dashboardmanu.map((entry, index) => (
                           <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}
-                          style={{
-                            filter: `drop-shadow(2px 2px 4px #5e5d5d`
-                          }} 
+                            style={{
+                              filter: `drop-shadow(2px 2px 4px #5e5d5d`
+                            }}
                           />
                         ))}
                       </Pie>
@@ -149,49 +144,51 @@ export default function AssetsDash(callback) {
                   </ResponsiveContainer>
                 </div>}
               <div >
-                <small className='text-secondary position-absolute mx-3' style={{ bottom:'6.5%' }}>Manufacturer</small>
+                <small className='text-secondary position-absolute mx-3' style={{ bottom: '6.5%' }}>Manufacturer</small>
               </div>
             </div>
-            <div className='software_graph' style={{height:"35vh"}}>
+            <div className='software_graph position-relative bg-white rounded shadow1-silver' style={{ height: "35vh" }}>
               {spinner ?
-                <div class="spinner-border text-warning" style={{ marginTop: "5%", marginLeft: "50%" }} role="status">
-                  <span class="sr-only"></span>
+                <div className="spinner-border text-warning" style={{ marginTop: "5%", marginLeft: "50%" }} role="status">
+                  <span className="sr-only"></span>
                 </div> :
-                <div className="soft_bar" style={{width:"94%" ,height:"90%"}}>
+                <div className="soft_bar position-absolute rounded">
                   <ResponsiveContainer>
-                    <BarChart data={dashboardsoft} margin={{ top: 18, right: 30, left: -20,bottom:9 }}>
+                    <BarChart data={dashboardsoft} margin={{ top: 18, right: 30, left: -20, bottom: 9 }}>
                       <CartesianGrid strokeDasharray='4' vertical={false} />
                       <XAxis tick={{ fill: 'white' }} dataKey="software_name" interval={"preserveStartEnd"} fontSize={12} />
                       <YAxis tick={{ fill: 'white' }} />
-                      <Tooltip contentStyle={{ backgroundColor: "rgba(41,40, 40,0.8)",color:"white",borderRadius:"3px",border:"1px solid rgba(41,40, 40)" }} />
+                      <Tooltip contentStyle={{ backgroundColor: "rgba(41,40, 40,0.8)", color: "white", borderRadius: "3px", border: "1px solid rgba(41,40, 40)" }} />
                       <Bar dataKey="software" fill="white" barSize={5} radius={30} />
                       <Tooltip />
                     </BarChart>
                   </ResponsiveContainer>
                 </div>
               }
-                <small className='position-absolute text-secondary mx-3' style={{bottom:'6.5%'}}>Software</small>
+              <small className='position-absolute text-secondary mx-3' style={{ bottom: '6.5%' }}>Software</small>
             </div>
           </div>
         </div>
-        <div className='asset-sidebar text-dark bg-white px-3 pt-2'>
-          <div className='Asset_card2 rounded  py-2'>
+
+        {/* ################ Assets Sidebar ##################### */}
+        <div className='asset-sidebar text-dark bg-white px-3 py-2 rounded'>
+          <div className='Asset_card2 my-1 mx-auto'>
             <p className='mb-0'>Asset Value</p>
             <h3>₹ {Assetsdata.PurchaseVal}</h3>
           </div>
-          <div className='Asset_card2'>
+          <div className='Asset_card2 my-1 mx-auto'>
             <p className='mb-0'>Rental / Month</p>
             <h3 >₹ {Assetsdata.RentMonth}</h3>
           </div>
-          <div className='Asset_card2'>
+          <div className='Asset_card2 my-1 mx-auto'>
             <p className='mb-0'>MS OS</p>
             <h3 >0</h3>
           </div>
-          <div className='Asset_card2'>
+          <div className='Asset_card2 my-1 mx-auto'>
             <p className='mb-0'>MS OS</p>
             <h3 className='pb-0'>0</h3>
           </div>
-          <div className='Asset_card2'>
+          <div className='Asset_card2 my-1 mx-auto'>
             <p className='mb-0'>MS OS</p>
             <h3 >0</h3>
           </div>
--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Sidebar from '../../Sidebar/Sidebar';
 import './Dashboard.css'
-import { BsLaptopFill } from 'react-icons/bs';
-import { HiUsers } from 'react-icons/hi';
-import { FaTicketAlt } from 'react-icons/fa';
-import { MdStickyNote2 } from 'react-icons/md';
 import AssetsDash from './AssetsDash'
 import VendorDash from './VendorDash/VendorDash'
 import Outstanding from './Outstanding/Outstanding'
@@ -14,7 +10,6 @@ import TickteDash from './TicketDash/TicketDash'
 import { DashboarDetails } from '../../../api/index'
 import LoadingPage from '../../LoadingPage/LoadingPage';
 import Recurring from './VendorDash/Recurring';
-import styled from 'styled-components';
 
 
 const Dashboard = () => {
@@ -23,44 +18,41 @@ const Dashboard = () => {
   const [dashboarddetails, setDashboarddetails] = useState({
     "Assetdata": 0,
     "Vendordata": 0,
-    "Invoice": 0,  
+    "Invoice": 0,
     "Ticket": 0,
   })
   useEffect(() => {
     const fetch = async () => {
       const org = localStorage.getItem('Database')
-
       const result = await DashboarDetails(org)
-      console.log(result)
       setLoading(true)
-
       setDashboarddetails({ ...dashboarddetails, Assetdata: result.Assets.asset, Vendordata: result.Vendor.Vendor_code, Invoice: result.Invoice.vendor, Ticket: result.Ticket.ticket })
     }
     fetch();
   }, [])
 
-  const colordynamic = (step) =>{
-      for(let i=1; i<=4;i++){
-        if(i==step){
-          console.log("true")
-          document.getElementById(`css${i}`).style.background = "white"
-          document.getElementById(`css${i}`).style.borderBottom = "4px solid #30305f"
-          document.getElementById(`css${i}`).style.color = "#595859"
+  const colordynamic = (step) => {
+    for (let i = 1; i <= 4; i++) {
+      if (i === step) {
+        console.log("true")
+        document.getElementById(`css${i}`).style.background = "white"
+        document.getElementById(`css${i}`).style.borderBottom = "4px solid #30305f"
+        document.getElementById(`css${i}`).style.color = "#595859"
 
-        }else{
-          document.getElementById(`css${i}`).style.background = "linear-gradient(45deg, rgb(85, 84, 82), rgb(121, 118, 113))"
-          document.getElementById(`css${i}`).style.color = "white"
-          document.getElementById(`css${i}`).style.borderBottom = "none"
-        }
+      } else {
+        document.getElementById(`css${i}`).style.background = "linear-gradient(45deg, #3d59e6, #603ae9)"
+        document.getElementById(`css${i}`).style.color = "white"
+        document.getElementById(`css${i}`).style.borderBottom = "none"
       }
+    }
   }
 
   const showStep = (step) => {
     switch (step) {
       case 1:
-        return <AssetsDash  />
+        return <AssetsDash />
       case 2:
-        return <VendorDash  setStep={setStep} />
+        return <VendorDash setStep={setStep} />
       case 3:
         return <Outstanding setStep={setStep} />
       case 4:
@@ -80,13 +72,12 @@ const Dashboard = () => {
         loading ?
           <Sidebar>
             <div className='dashboard_container' >
-
-              <div className='dashboard_cards justify-content-end' >
-                <div className='d-flex justify-content-center  dash_toggle_btns'>
-                  <div className='dash_toggle_btn ' style={{background:"white",borderBottom:"4px solid #30305f",color:"#595859"}} id="css1"  onClick={() => {colordynamic(1); setStep(1)}}>Assets</div>
-                  <div className='dash_toggle_btn ' id="css2" onClick={() =>{colordynamic(2); setStep(2)}}>Vendors</div>
-                  <div className='dash_toggle_btn ' id="css3" onClick={() => {colordynamic(3);setStep(3)}}>Invoice</div>
-                  <div className='dash_toggle_btn ' id="css4" onClick={() => {colordynamic(4);setStep(4)}}>Tickets</div>
+              <div className='dashboard_cards d-flex justify-content-end' >
+                <div className='d-flex justify-content-center  dash_toggle_btns rounded'>
+                  <div className='dash_toggle_btn cursor-pointer ' style={{ background: "white", borderBottom: "4px solid #30305f", color: "#595859" }} id="css1" onClick={() => { colordynamic(1); setStep(1) }}>Assets</div>
+                  <div className='dash_toggle_btn cursor-pointer' id="css2" onClick={() => { colordynamic(2); setStep(2) }}>Vendors</div>
+                  <div className='dash_toggle_btn cursor-pointer' id="css3" onClick={() => { colordynamic(3); setStep(3) }}>Invoice</div>
+                  <div className='dash_toggle_btn cursor-pointer' id="css4" onClick={() => { colordynamic(4); setStep(4) }}>Tickets</div>
                 </div>
               </div>
               {showStep(currentStep)}
