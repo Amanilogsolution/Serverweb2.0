debdc96a457aa2c95196397c984e261c287e18e8
rupeshIlogsolution <rupesh.kumar@ilogsolution.com>
2023-01-25 17:34:14 +0530

    Dashboard Complete

--- src/components/pages/Dashboard/VendorDash/Recurring.js
+++ src/components/pages/Dashboard/VendorDash/Recurring.js
@@ -3,11 +3,11 @@ import './Recurring.css'
 import ReactPaginate from 'react-paginate';
 import { Recurring_Vendor, Recurring_Frequency } from '../../../../api/index'
 import { CSVLink } from "react-csv";
-import {BiExport} from 'react-icons/bi'
-import {SiMicrosoftexcel} from 'react-icons/si'
-import {GrDocumentCsv} from 'react-icons/gr'
-import {ExcelConvertData} from './Excel'
-import { IoMdArrowDropleft,IoMdArrowDropright } from 'react-icons/io';
+import { BiExport } from 'react-icons/bi'
+import { SiMicrosoftexcel } from 'react-icons/si'
+import { GrDocumentCsv } from 'react-icons/gr'
+import { ExcelConvertData } from './Excel'
+import { IoMdArrowDropleft, IoMdArrowDropright } from 'react-icons/io';
 
 
 
@@ -68,15 +68,15 @@ export default function Recurring() {
     <div className='Recurring_div justify-content-around mx-2'>
 
       <div className='recurring_table position-relative bg-light rounded' >
-      <div className="recurringhead position-absolute  d-flex justify-content-between pt-3">
-      <p className='text-white px-4 mx-1'>Vendor Recurring Details</p>
-      <div title="Export" className="d-flex justify-content-end mr-2 cursor-pointer  px-3" onClick={(e) => { e.preventDefault(); setToogle(value => !value) }} style={{width:'5%',float:'right'}}>
-            <BiExport style={{ fontSize: "25px",color: "white" }} />
+        <div className="recurringhead position-absolute px-3  d-flex justify-content-between pt-3">
+          <p className='text-white px-2 mx-1'>Vendor Recurring Details</p>
+          <div title="Export" className="d-flex justify-content-end text-white cursor-pointer" onClick={(e) => { e.preventDefault(); setToogle(value => !value) }} >
+            <BiExport style={{ fontSize: "25px" }} />
           </div>
 
           {
             toogle ?
-              <div className="d-flex flex-column justify-content-center align-items-center bg-light position-absolute rounded py-1" style={{ right: "4%",top:'25%', width: "5%", boxShadow: "3px 3px 10px black" }}>
+              <div className="d-flex flex-column justify-content-center align-items-center bg-light position-absolute rounded py-1" style={{ right: "4%", top: '25%', width: "5%", boxShadow: "3px 3px 10px black" }}>
                 <a href="#"
                   onClick={exportExcel}
                 ><SiMicrosoftexcel className='ft-20' /></a>
@@ -89,79 +89,79 @@ export default function Recurring() {
               : ''
           }
 
-      </div>
+        </div>
 
         <div className='recurring_table_inside px-3'>
-          
+
           <div id="pagination">
-          <table className="table " >
-            <thead>
-              <tr>
-                <th scope="col">Vendor</th>
-                <th scope="col">Location</th>
-                <th scope="col">Major Category</th>
-                <th scope="col">Sub Category</th>
-                <th scope="col">Account no</th>
-                <th scope="col">Reference No</th>
-                <th scope="col">Help Desk no</th>
-                <th scope="col">Billing Frequency</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                Recurringdata.map((elements,index) => {
-                  return (
-                    <tr key={index}>
-                      <td>{elements.vendor}</td>
-                      <td>{elements.location}</td>
-                      <td>{elements.major_category}</td>
-                      <td>{elements.sub_category}</td>
-                      <td>{elements.customer_account_no}</td>
-                      <td>{elements.reference_no}</td>
-                      <td>{elements.help_desk_no}</td>
-                      <td>{elements.billling_freq}</td>
-                    </tr>
-                  )
-                })
-              }
-            </tbody>
-          </table>
+            <table className="table " >
+              <thead>
+                <tr>
+                  <th scope="col">Vendor</th>
+                  <th scope="col">Location</th>
+                  <th scope="col">Major Category</th>
+                  <th scope="col">Sub Category</th>
+                  <th scope="col">Account no</th>
+                  <th scope="col">Reference No</th>
+                  <th scope="col">Help Desk no</th>
+                  <th scope="col">Billing Frequency</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  Recurringdata.map((elements, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{elements.vendor}</td>
+                        <td>{elements.location}</td>
+                        <td>{elements.major_category}</td>
+                        <td>{elements.sub_category}</td>
+                        <td>{elements.customer_account_no}</td>
+                        <td>{elements.reference_no}</td>
+                        <td>{elements.help_desk_no}</td>
+                        <td>{elements.billling_freq}</td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
           </div>
-         
+
         </div>
         <div className="for_pnation justify-content-end mx-3">
-            <div className='rows_per_page mx-4'>
-              <label className='mx-2'>Rows / page</label>
-              <select onChange={handleChange}>
-                <option value="10">10</option>
-                <option value="15">15</option>
-                <option value="20">20</option>
+          <div className='rows_per_page mx-4'>
+            <label className='mx-2'>Rows / page</label>
+            <select onChange={handleChange}>
+              <option value="10">10</option>
+              <option value="15">15</option>
+              <option value="20">20</option>
 
-              </select>
-            </div>
-            <div className='react_pagination'>
-              <ReactPaginate
-                breakLabel="..."
-                nextLabel={<IoMdArrowDropright style={{fontSize:"24px"}}/>}
-                onPageChange={handlePageClick}
-                pageRangeDisplayed={3}
-                pageCount={lastval}
-                previousLabel={<IoMdArrowDropleft style={{fontSize:"24px"}}/>}
-                renderOnZeroPageCount={null}
-                containerClassName={'pagination justify-content-end'}
-                pageClassName={'page-item'}
-                pageLinkClassName={'page-link'}
-                previousClassName={'page-item'}
-                previousLinkClassName={'page-link'}
-                nextClassName={'page-item'}
-                nextLinkClassName={'page-link'}
-                breakClassName={'page-item'}
-                breakLinkClassName={'page-link'}
-                activeClassName={'active'}
-              />
-            </div>
+            </select>
           </div>
-        
+          <div className='react_pagination'>
+            <ReactPaginate
+              breakLabel="..."
+              nextLabel={<IoMdArrowDropright style={{ fontSize: "24px" }} />}
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={3}
+              pageCount={lastval}
+              previousLabel={<IoMdArrowDropleft style={{ fontSize: "24px" }} />}
+              renderOnZeroPageCount={null}
+              containerClassName={'pagination justify-content-end'}
+              pageClassName={'page-item'}
+              pageLinkClassName={'page-link'}
+              previousClassName={'page-item'}
+              previousLinkClassName={'page-link'}
+              nextClassName={'page-item'}
+              nextLinkClassName={'page-link'}
+              breakClassName={'page-item'}
+              breakLinkClassName={'page-link'}
+              activeClassName={'active'}
+            />
+          </div>
+        </div>
+
       </div>
       <div className='recurring_cards bg-light p-2 rounded'>
         <div className='recurring_card text-center text-light pt-2 rounded mb-2 '>
