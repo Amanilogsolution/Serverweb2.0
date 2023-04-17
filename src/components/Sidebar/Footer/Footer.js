c970b4d78cb7a8c1da2112fe071e7afcbd0e0f08
Rupesh <rupesh.kumar@ilogsolution.com>
2023-04-17 11:51:50 +0530

    Done

--- src/components/Sidebar/Footer/Footer.js
+++ src/components/Sidebar/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <>
             <div className="footer mt-4 pt-3 d-flex justify-content-between px-4 ">
-                <h6 style={{ color: "#424242" }}>Copyright © 2022 Drizzle. All rights reserved</h6>
+                <h6 style={{ color: "#424242" }}>Copyright © 2023 Drizzle. All rights reserved</h6>
                 <div className='social_media_links'>
                     <Link to='' className='text-decoration-none'><AiOutlineTwitter style={{ fontSize: "27px" }} /></Link>
                     <Link to='' className='mx-3 text-decoration-none'><BsFacebook style={{ fontSize: "24px" }} /></Link>
